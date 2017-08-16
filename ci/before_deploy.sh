fb5cfef6da14da22f5095700faa19dd32d5dc4c9
Liam Dawson <dawmail333@gmail.com>
2017-08-17 09:50:46 +1000

    remove version from travis CI build archives

--- ci/before_deploy.sh
+++ ci/before_deploy.sh
@@ -19,11 +19,10 @@ main() {
 
     cross rustc --bin pogpen --target $TARGET --release -- -C lto
 
-    # TODO Update this to package the right artifacts
     cp target/$TARGET/release/pogpen $stage/
 
     cd $stage
-    tar czf $src/$CRATE_NAME-$TRAVIS_TAG-$TARGET.tar.gz *
+    tar czf $src/$CRATE_NAME-$TARGET.tar.gz *
     cd $src
 
     rm -rf $stage
